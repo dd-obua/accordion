e37e3f51257c443386d07fd6cfea436e4aed318b
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-13 13:43:35 +0300

    Add open class conditionally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,10 +22,10 @@ const Accordion = ({ data }) => {
 const AccordionItem = ({ num, title, text }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => setIsOpen((is) => !is);
+  const handleToggle = () => setIsOpen((val) => !val);
 
   return (
-    <div className="item" onClick={handleClick}>
+    <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
       <span className="number">{num < 9 ? `0${num + 1}` : num + 1}</span>
       <h2 className="title">{title}</h2>
       <span className="icon">{isOpen ? '-' : '+'}</span>
